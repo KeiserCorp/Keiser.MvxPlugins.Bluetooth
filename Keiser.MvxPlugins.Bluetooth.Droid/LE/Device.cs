901246a180de07de7f1ea05551ae2b22a9c6a9d0
Zach Cardoza <bayssmekanique@live.com>
2014-12-10 23:31:26 -0800

    Fixes Droid.LE Device Advertisement Data
    Byte array now returns only advertisement data portion (starting with
    data type value)

--- Keiser.MvxPlugins.Bluetooth.Droid/LE/Device.cs
+++ Keiser.MvxPlugins.Bluetooth.Droid/LE/Device.cs
@@ -2,12 +2,13 @@ namespace Keiser.MvxPlugins.Bluetooth.Droid.LE
 {
     using Android.Bluetooth;
     using Keiser.MvxPlugins.Bluetooth.LE;
+    using System;
 
     public class Device : DeviceBase
     {
         protected BluetoothDevice _nativeDevice;
         protected int _rssi;
-        protected byte[] _scanRecord;
+        protected byte[] _advertisementData;
 
         public Device(BluetoothDevice nativeDevice, int rssi, byte[] scanRecord)
         {
@@ -18,13 +19,15 @@ namespace Keiser.MvxPlugins.Bluetooth.Droid.LE
 
         protected void SetAdvertisingData(byte[] scanRecord)
         {
-            string print = "AdvData: ";
-            foreach (var value in scanRecord)
-            {
-                print += "[" + value.ToString() + "] ";
-            }
-            Keiser.MvxPlugins.Bluetooth.Trace.Info(print);
-            _scanRecord = scanRecord;
+            int startingIndex = 0;
+            // Name Section
+            startingIndex += scanRecord[startingIndex] + 1;
+            // Flags Section
+            startingIndex += scanRecord[startingIndex] + 1;
+            // Advertisment Data Section
+            int length = scanRecord[startingIndex++];
+            _advertisementData = new byte[length];
+            Array.Copy(scanRecord, startingIndex, _advertisementData, 0, length);
         }
 
         public override Address ID
@@ -55,7 +58,7 @@ namespace Keiser.MvxPlugins.Bluetooth.Droid.LE
         {
             get
             {
-                return _scanRecord;
+                return _advertisementData;
             }
         }
 
